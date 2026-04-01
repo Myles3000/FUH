c7ef69438c3ed531142aa2e1a77f19baf2d1e3ab
Dagmawet <dagmawetz@gmail.com>
2026-04-01 12:03:48 -0500

    Initial commit

---
+++ Assets/Scripts/Outliner.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+
+public class Outliner : MonoBehaviour
+{
+
+    public Outline outline;
+
+    void Start()
+    {
+        //get outline
+        if (!outline) outline = GetComponent<Outline>();
+
+        //keep outline off until something has been hit 
+        if (outline) outline.enabled = false;
+    }
+
+    void Update()
+    {
+        //if ray hits object, turn on highlighter 
+        if (outline)
+            outline.enabled = raycaster.ray && raycaster.ray.LookingAt(transform);
+    }
+
+
+}
